4eec04ca039f1367bfe325ae881e42021e12d59b
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-01-13 10:01:28 -0500

    Add files via upload

---
+++ C#/IntroToLogic/rectangle/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Rectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the width of the rectangle?"); double width =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the length of the rectangle?"); double Length=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The perimeter is "+Convert.ToString(2*width+2*Length));
+            Console.WriteLine("The area is " + Convert.ToString(width * Length));
+        }
+    }
+}
