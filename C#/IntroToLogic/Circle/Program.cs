a1293a7ed4d22586c81b2604631cc3f5f087143e
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-01-13 12:03:00 -0500

    Add files via upload

---
+++ C#/IntroToLogic/Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What would you like this circle's radius to be?"); double radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The area of the circle is "+ Convert.ToString(Math.PI* Math.Pow(radius, 2)));
+            Console.WriteLine("The Circumference is " + Convert.ToString(2 * Math.PI * radius));
+        }
+    }
+}
