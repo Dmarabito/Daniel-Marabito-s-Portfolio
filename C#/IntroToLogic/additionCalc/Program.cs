b7f7d5fe20532eba48f772989928ceeb728c24f0
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-01-13 09:30:54 -0500

    Add files via upload

---
+++ C#/IntroToLogic/additionCalc/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace AdditionCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a number");
+            double Value1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Give me another");
+            double Value2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(Value1+Value2);
+        }
+    }
+}
