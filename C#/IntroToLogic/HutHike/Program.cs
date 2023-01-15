23d369c1243fefd57e7db068d3f8e11aba8a8830
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-01-15 10:28:14 -0500

    Add files via upload

---
+++ C#/IntroToLogic/HutHike/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace HutHike
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++) 
+            {
+                if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.WriteLine("hutt and hike");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("hutt");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.WriteLine("hike");
+                }
+                else 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
