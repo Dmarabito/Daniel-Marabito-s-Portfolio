17788c45c8a7d16400c8c9bb179d58365e7d33aa
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-01-19 19:14:18 -0500

    Add files via upload

---
+++ C#/IntroToLogic/HighLowGuess/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace HighLowGuessingGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumberOfGuesses = 0;
+            Random Randy = new Random();
+            int RandomNumber = (Randy.Next()%10)+1;
+            string Guess = "";
+            while(Guess != Convert.ToString(RandomNumber))
+            {
+                Console.WriteLine("What is you Guess"); Guess = Console.ReadLine();
+                if (Convert.ToInt64(Guess) < RandomNumber) 
+                {
+                    NumberOfGuesses += 1;
+                    Console.WriteLine("You are low");
+                }
+                else if(Convert.ToInt64(Guess) > RandomNumber)
+                {
+                    NumberOfGuesses += 1;
+                    Console.WriteLine("You are high");
+                }
+                else if(Convert.ToInt64(Guess) == RandomNumber)
+                {
+                    NumberOfGuesses += 1;
+                    Console.WriteLine("You got it right in "+Convert.ToString(NumberOfGuesses)+" tries");
+                }
+            }
+        }
+    }
+}
