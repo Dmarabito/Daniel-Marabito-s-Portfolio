25cfeec252ef5e79cdda36b411cb166b055fc161
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-10-10 15:19:10 -0400

    Add files via upload

---
+++ sql/IT2351EnterpriseDatabaseSystems/Assignment6ModifyTables/DMAssignment6P6ModifyingTables.sql
@@ -0,0 +1,6 @@
+-- Daniel Marabito
+
+delete from courses where course_number = 1000 or course_number = 1010;
+select * from courses;
+
+/*Finally, let’s try to delete courses 1000 and 1010. What happens? Why?*/
