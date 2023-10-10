3dba34c56dea1440a047a16da6a2bae9a9833b86
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-10-10 14:18:22 -0400

    Add files via upload

---
+++ sql/IT2351EnterpriseDatabaseSystems/Assignment6ModifyTables/DMAssignment6P2ModifyingTables.sql
@@ -0,0 +1,7 @@
+-- Daniel Marabito
+-- Just do it in order of the table, seperate each set of values with comma.
+insert into courses
+values (1200, "History 101","Intro History", 4),
+(1210, "History 102","World History", 4),
+(1220, "History 103","Archaeology",Null);
+select * from courses;
