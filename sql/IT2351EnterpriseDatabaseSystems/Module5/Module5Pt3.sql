e0727ae7579de56d11a78d2a425a49d014795ff0
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2023-10-17 15:16:13 -0400

    Add files via upload

---
+++ sql/IT2351EnterpriseDatabaseSystems/Module5/Module5Pt3.sql
@@ -0,0 +1,5 @@
+-- Daniel Marabito
+alter table employeeplural -- Again went with employeeplural since I am assuming it is suppose to be the table from previous part but one already exists prior to this.
+add email varchar(30); -- added email.
+
+Select * from employeeplural;
