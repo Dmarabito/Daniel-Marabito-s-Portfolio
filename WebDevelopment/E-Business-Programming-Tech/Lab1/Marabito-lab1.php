68f6e27c24a1f603681502e00ea0aa77a01e648d
Dmarabito <114103013+Dmarabito@users.noreply.github.com>
2024-09-17 12:34:19 -0400

    Add files via upload

---
+++ WebDevelopment/E-Business-Programming-Tech/Lab1/Marabito-lab1.php
@@ -0,0 +1,21 @@
+<?php
+	// Create a variable called myName and assign it a string with your your name
+	$myName = "Daniel Marabito";
+	
+	// Create a variable called myMajor and assign it a string with your your major
+	$myMajor = "Information Technology - Programming and Development, Associate of Applied Business";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<title>IT 2600 - Lab 1</title>
+</head>
+<body>
+	<!-- Replace the word PHPCODE with an embedded PHP statement that prints out the myName variable -->
+	<h2><?php echo $myName; ?> - IT 2600 - Lab 1</h2>
+	<!-- Replace the word PHPCODE with an embedded PHP statement that prints out the myMajor variable -->
+	<h3>My major is <?php echo $myMajor ?></h3>
+</body>
+</html>
+	
